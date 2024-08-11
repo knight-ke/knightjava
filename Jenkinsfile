09cce5a58985458e56e12d58db6cd17d4085a9c5
kexsh <769014005@qq.com>
2024-08-11 23:12:46 +0800

    增加单元测试

--- Jenkinsfile
+++ Jenkinsfile
@@ -13,5 +13,15 @@ pipeline {
                 sh 'mvn package'
             }
         }
+        stage('Test') {
+            steps {
+                sh 'mvn test'
+            }
+            post {
+                always {
+                    junit 'target/surefire-reports/*.xml'
+                }
+            }
+        }
     }
 }
---
+++ src/test/com/knight/bean/UserTest.java
@@ -0,0 +1,50 @@
+package com.knight.bean;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getUserId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getAge() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setUserId() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setAge() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+}
